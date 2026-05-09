43302776549db6e69b3a3b395a648420cdae8184
obararyoinfo <obararyo.info@gmail.com>
2026-05-10 00:51:03 +0900

    first commit

--- data/discography.js
+++ data/discography.js
@@ -3,13 +3,13 @@
 var DISCOGRAPHY_DATA = {
   "releases": [
     {
-      "id": 7,
+      "id": 16,
       "type": "Digital Single",
-      "label": "7th Digital Single",
+      "label": "6th Digital Single",
       "title": "水彩、北浜、午後11時",
       "date": "2026-04-17",
-      "catalog": "KYOSO-007",
-      "image": "images/releases/007_suisai.jpg",
+      "catalog": "",
+      "image": "images/releases/S08_suisai.jpg",
       "tracks": [
         { "number": 1, "title": "水彩、北浜、午後11時" }
       ],
@@ -18,13 +18,13 @@ var DISCOGRAPHY_DATA = {
       "comment": ""
     },
     {
-      "id": 6,
+      "id": 15,
       "type": "Digital Single",
-      "label": "6th Digital Single",
+      "label": "5th Digital Single",
       "title": "カッコウの鳴く頃に",
       "date": "2026-01-17",
-      "catalog": "KYOSO-006",
-      "image": "",
+      "catalog": "",
+      "image": "images/releases/S07_kakkou.jpg",
       "tracks": [
         { "number": 1, "title": "カッコウの鳴く頃に" }
       ],
@@ -33,19 +33,148 @@ var DISCOGRAPHY_DATA = {
       "comment": ""
     },
     {
-      "id": 2,
+      "id": 14,
       "type": "Compilation Album",
       "label": "2nd Compilation Album",
       "title": "NO VISUAL, NO LIFE 〜SALVATIONIS〜",
       "date": "2025-11-29",
       "catalog": "",
-      "image": "images/releases/A02_suiyou.jpg",
+      "image": "images/releases/A03_suiyou.jpg",
       "tracks": [
         { "number": 1, "title": "新約、水曜に見る夢" }
       ],
-      "purchase_url": "https://t.co/FXIB8ZE02d",
+      "purchase_url": "https://t.co/B9B5rMux63",
       "streaming_url": "",
-      "comment": "昨年に引き続き参加。狂想ドッペルでご購入の方には特典「3track音源+限定コースター」をプレゼント"
+      "comment": "狂想ドッペルでご購入の方には特典「3track音源+限定コースター」プレゼント!"
+    },
+    {
+      "id": 13,
+      "type": "Digital Single",
+      "label": "4th Digital Single",
+      "title": "メイデイ",
+      "date": "2025-05-23",
+      "catalog": "",
+      "image": "images/releases/S06_mayday.jpg",
+      "tracks": [
+        { "number": 1, "title": "メイデイ" }
+      ],
+      "purchase_url": "",
+      "streaming_url": "https://t.co/OGKMq3T9zi",
+      "comment": "3ヶ月連続リリース3本目!"
+    },
+    {
+      "id": 12,
+      "type": "Digital Single",
+      "label": "3rd Digital Single",
+      "title": "孤独讃歌",
+      "date": "2025-04-25",
+      "catalog": "",
+      "image": "images/releases/S05_kodoku.jpg",
+      "tracks": [
+        { "number": 1, "title": "孤独讃歌" }
+      ],
+      "purchase_url": "",
+      "streaming_url": "https://t.co/bnSo6FIJZL",
+      "comment": "3ヶ月連続リリース2本目!"
+    },
+    {
+      "id": 11,
+      "type": "Digital Single",
+      "label": "2nd Digital Single",
+      "title": "衝動的ヘヴン",
+      "date": "2025-03-21",
+      "catalog": "",
+      "image": "images/releases/S04_syodo.jpg",
+      "tracks": [
+        { "number": 1, "title": "衝動的ヘヴン" }
+      ],
+      "purchase_url": "",
+      "streaming_url": "https://t.co/birSdEIQ0Y",
+      "comment": "3ヶ月連続リリース1本目!"
+    },
+    {
+      "id": 10,
+      "type": "Compilation Album",
+      "label": "1st Compilation Album",
+      "title": "NO VISUAL, NO LIFE〜COUNTER-CLOCKWISE〜",
+      "date": "2024-12-01",
+      "catalog": "",
+      "image": "images/releases/A02_suisai.jpg",
+      "tracks": [
+        { "number": 1, "title": "水彩、北浜、午後11時" }
+      ],
+      "purchase_url": "https://t.co/sDs4MxPqmt",
+      "streaming_url": "",
+      "comment": "狂想ドッペルでご購入の方には特典「豪華6曲入り音源」プレゼント!"
+    },
+    {
+      "id": 9,
+      "type": "Single",
+      "label": "2nd Single",
+      "title": "フィクション≒ノンフィクション",
+      "date": "2023-12-01",
+      "catalog": "",
+      "image": "images/releases/S03_fiction.jpg",
+      "tracks": [
+        { "number": 1, "title": "フィクション≒ノンフィクション" },
+        { "number": 2, "title": "Re:創狂（LIVE AT 寺田町Fireloop 2023.10.7）" },
+        { "number": 3, "title": "天国での審判" },
+      ],
+      "purchase_url": "https://dedshop.base.shop/items/80196135",
+      "streaming_url": "https://t.co/qMYc8KkOY7",
+      "comment": "Track2,3はディスク版のみ収録"
+    },
+    {
+      "id": 8,
+      "type": "Single",
+      "label": "1st Single",
+      "title": "長堀橋エレクトリックラプソディ",
+      "date": "2023-09-30",
+      "catalog": "",
+      "image": "images/releases/S02_nagahori.jpg",
+      "tracks": [
+        { "number": 1, "title": "長堀橋エレクトリックラプソディ" },
+        { "number": 2, "title": "ララバイ（LIVE AT 神戸PADOMA 2023.4.16）" },
+        { "number": 3, "title": "ジャパニーズサバイヴ デジタルエンドロール（remix）" },
+      ],
+      "purchase_url": "https://dedshop.base.shop/items/77150324",
+      "streaming_url": "https://t.co/vpj1fCQsNs",
+      "comment": "Track2,3はディスク版のみ収録"
+    },
+    {
+      "id": 7,
+      "type": "Mini Album",
+      "label": "1st Mini Album",
+      "title": "ひかりのようなもの",
+      "date": "2022-10-23",
+      "catalog": "",
+      "image": "images/releases/A01_hikari.jpg",
+      "tracks": [
+        { "number": 1, "title": "5:10 p.m" },
+        { "number": 2, "title": "夏の日のこと" },
+        { "number": 3, "title": "1.19 a.m" },
+        { "number": 4, "title": "ララバイ" },
+        { "number": 5, "title": "4:45 a.m" },
+        { "number": 6, "title": "明け方、空虚の先" },
+      ],
+      "purchase_url": "https://dedshop.base.shop/items/67606649",
+      "streaming_url": "https://t.co/a9nSBzNlMP",
+      "comment": "Track1,3,5はディスク版のみ収録"
+    },
+    {
+      "id": 8,
+      "type": "Digital Single",
+      "label": "1st Digital Single",
+      "title": "Re:創狂",
+      "date": "2022-04-30",
+      "catalog": "",
+      "image": "images/releases/S01_risou.jpg",
+      "tracks": [
+        { "number": 1, "title": "Re:創狂" }
+      ],
+      "purchase_url": "",
+      "streaming_url": "https://linkco.re/qhUG9YAd?lang=ja",
+      "comment": ""
     }
   ]
 };
